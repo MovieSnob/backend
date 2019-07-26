77ebe4420ebdf9c784c8b4aae699b52908e61f3b
Maxim Filippov <colixer@gmail.com>
2019-07-26 16:10:28 +0300

    Scope all requests under /api

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,28 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount ActionCable.server => '/cable'
+  namespace :api do
+    mount ActionCable.server => '/cable'
 
-  post 'authenticate', to: 'authentication#authenticate'
-  post 'register', to: 'authentication#register'
+    post 'authenticate', to: 'authentication#authenticate'
+    post 'register', to: 'authentication#register'
 
-  resources :users, only: %i[index] do
-    collection do
-      get 'me'
-    end
-  end
-
-  resources :films, only: %i[create index] do
-    collection do
-      get 'suggested'
+    resources :users, only: %i[index] do
+      collection do
+        get 'me'
+      end
     end
 
-    member do
-      post 'watched', to: 'films#mark_watched'
-      post 'unwatched', to: 'films#mark_unwatched'
-      patch 'score'
+    resources :films, only: %i[create index] do
+      collection do
+        get 'suggested'
+      end
+
+      member do
+        post 'watched', to: 'films#mark_watched'
+        post 'unwatched', to: 'films#mark_unwatched'
+        patch 'score'
+      end
     end
   end
 end
