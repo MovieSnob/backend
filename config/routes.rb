782d50c2d24c1d07320e7af013bb14fb529beacf
Maxim Filippov <colixer@gmail.com>
2019-07-26 16:13:12 +0300

    Scope, not namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace :api do
+  scope '/api' do
     mount ActionCable.server => '/cable'
 
     post 'authenticate', to: 'authentication#authenticate'
